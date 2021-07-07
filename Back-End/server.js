e6467441dc2fecbd0a6cec4d683dca43dea85951
Lenny Marcel <lennyfmarcel@gmail.com>
2021-07-07 12:13:17 +0200

    test back link bdd

--- Back-End/server.js
+++ Back-End/server.js
@@ -11,7 +11,7 @@ server.use(morgan("common"));
 // --------------- CORS ---------------
 
 const whitelist = [
-  'http://localhost:4200'
+  '127.0.0.1'
 ];
 server.options('*', cors());
 server.use(cors((req, callback) => {
