cc21dadf3d69c041d10be78dc90246297061a1ff
Lenny Marcel <lennyfmarcel@gmail.com>
2021-07-07 12:17:33 +0200

    test bis

--- Back-End/configs/config.js
+++ Back-End/configs/config.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 // Constants
 module.exports = {
   database: {
-    host:"localhost",
+    host:"185.171.202.192",
     port: 49174,
     database: 'AtypikHouse',
     user: 'atplead',
--- Back-End/server.js
+++ Back-End/server.js
@@ -11,7 +11,7 @@ server.use(morgan("common"));
 // --------------- CORS ---------------
 
 const whitelist = [
-  '127.0.0.1'
+  'http://localhost:4200'
 ];
 server.options('*', cors());
 server.use(cors((req, callback) => {
