abc3b9e1990df393720c744a370af969d3f1c4e8
Lenny Marcel <lennyfmarcel@gmail.com>
2021-08-10 14:40:32 +0200

    Fix string to text type. Fix +255 JWT

--- Back-End/configs/knex/migrations/20210809161757_create_users_table.js
+++ Back-End/configs/knex/migrations/20210809161757_create_users_table.js
@@ -3,13 +3,13 @@ exports.up = function(knex, Promise) {
   
     return knex.schema.createTable('users', function(table) {
         table.increments('usr_id');
-        table.string('usr_mail').notNullable();
-        table.string('usr_password').notNullable();
-        table.string('usr_firstName');
-        table.string('usr_lastName');
-        table.string('usr_access_token');
-        table.string('usr_refresh_token');
-        table.string('usr_expires_in');
+        table.text('usr_mail').notNullable();
+        table.text('usr_password').notNullable();
+        table.text('usr_firstName');
+        table.text('usr_lastName');
+        table.text('usr_access_token');
+        table.text('usr_refresh_token');
+        table.text('usr_expires_in');
 
         
       })
