e0271444c08e69dc6145e2cfd8c55a620ba7bfab
Lenny Marcel <lennyfmarcel@gmail.com>
2021-08-10 14:31:56 +0200

    update users bdd migration

--- Back-End/configs/knex/migrations/20210809161757_create_users_table.js
+++ Back-End/configs/knex/migrations/20210809161757_create_users_table.js
@@ -5,6 +5,12 @@ exports.up = function(knex, Promise) {
         table.increments('usr_id');
         table.string('usr_mail').notNullable();
         table.string('usr_password').notNullable();
+        table.string('usr_firstName');
+        table.string('usr_lastName');
+        table.string('usr_access_token');
+        table.string('usr_refresh_token');
+        table.string('usr_expires_in');
+
         
       })
 
