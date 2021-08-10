543b70347799d8d0dbe4108923e8b5652ba53d81
Lenny Marcel <lennyfmarcel@gmail.com>
2021-08-10 14:59:34 +0200

    reservation init

---
+++ Back-End/configs/knex/migrations/20210810124743_create_adverts_table.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex, Promise) {
+  
+    return knex.schema.createTable('adverts', function(table) {
+        table.increments('adv_id');
+        table.text('adv_name').notNullable();
+        table.bigint('adv_usr_id').notNullable();
+        table.bool('adv_status').notNullable();
+        table.bigint('adv_estate_type').notNullable();
+        
+
+        
+    })
+
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('users');
+};
---
+++ Back-End/models/advert.js
@@ -0,0 +1,11 @@
+var Advert = function (user) {
+    this.adv_id = advert.adv_id;
+    this.adv_name = advert.adv_name;
+    this.adv_usr_id = advert.adv_usr_id;
+    this.adv_status = advert.adv_status;
+    this.adv_estate_type = advert.adv_estate_type;
+
+};
+
+module.exports = Advert;
+
