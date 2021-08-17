0b5956be4d021c899185fd7d6aee6a03c78eab19
RemiSchneider76 <remischneider76@gmail.com>
2021-08-17 18:37:30 +0200

    add details and new annoucement

---
+++ Frontend/src/app/new-annoucement/new-annoucement.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NewAnnoucementComponent } from './new-annoucement.component';
+
+describe('NewAnnoucementComponent', () => {
+  let component: NewAnnoucementComponent;
+  let fixture: ComponentFixture<NewAnnoucementComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NewAnnoucementComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NewAnnoucementComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
