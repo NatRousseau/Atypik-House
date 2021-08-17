6df92943cbb8fad1962eb7d52e7cc4d039275c88
Lenny Marcel <lennyfmarcel@gmail.com>
2021-08-17 19:34:46 +0200

    quick fixs

--- Back-End/routes/userRoutes.js
+++ Back-End/routes/userRoutes.js
@@ -1,31 +1,12 @@
 module.exports = function (app) {
     //const auth = require('../middleware/auth');
     const userController = require('../controllers/userController');
-    const advertController = require('../controllers/advertController');
-
-
 //======================== USER  ==========================//
     app.route('/register')
         .post(userController.register);
 
     app.route('/login')
         .get(userController.login);
-
-//======================== Advert ==========================//
-
-    app.route('/createAdvert')
-        .post(userController.createAdvert);
-
-    app.route('/getAdvert')
-        .get(userController.getAdvert);
-
-    app.route('/updateAdvert')
-        .get(userController.updateAdvert);
-
-    app.route('/deleteAdvert')
-        .get(userController.deleteAdvert);
-
-    
 
 //======================== JWT TOKKENS ==========================//
 
