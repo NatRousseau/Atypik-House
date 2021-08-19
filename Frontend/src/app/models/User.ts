9757b580887b4d5a4cab73ed3f518fa81de7c39c
RemiSchneider76 <remischneider76@gmail.com>
2021-08-19 14:30:45 +0200

    patch router

--- Frontend/src/app/models/User.ts
+++ Frontend/src/app/models/User.ts
@@ -1,10 +1,10 @@
 export interface User {
-    usr_id:number
-    usr_mail:string
-    usr_password:string
-    usr_firstName:string
-    usr_lastName:string
-    usr_access_token: string
-    usr_refresh_token:string
-    usr_expires_in:string
+    usr_id:number;
+    usr_mail:string;
+    usr_password:string;
+    usr_firstName:string;
+    usr_lastName:string;
+    usr_access_token: string;
+    usr_refresh_token:string;
+    usr_expires_in:string;
 }
