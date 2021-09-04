225d73e1b9dcbf2d5b690a1fb8a049a1e568a804
NatRousseau <nathan.rousseau@viacesi.fr>
2021-09-05 00:44:14 +0200

    Navbar Account + dropdown

--- Frontend/src/app/navbar/sidenav-list/sidenav-list.component.ts
+++ Frontend/src/app/navbar/sidenav-list/sidenav-list.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
-import { SnackBarService } from '../../_services/SnackBar/snack-bar.service';
-import { StatusUser } from '../../_services/User/statusUser';
-import { UserService } from '../../_services/User/user.service';
+import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import {SnackBarService} from '../../_services/SnackBar/snack-bar.service';
+import {StatusUser} from '../../_services/User/statusUser';
+import {UserService} from '../../_services/User/user.service';
 
 @Component({
     selector: 'sidenav-list',
@@ -10,7 +10,12 @@ import { UserService } from '../../_services/User/user.service';
 })
 export class SidenavListComponent implements OnInit {
     user = localStorage.getItem('User');
-    constructor(public auth: UserService, public statusUser: StatusUser) {}
+
+    firstName: string = localStorage.getItem('usr_firstName');
+    lastName: string = localStorage.getItem('usr_lastName');
+
+    constructor(public auth: UserService, public statusUser: StatusUser) {
+    }
 
     @Output() sidenavClose = new EventEmitter();
 
